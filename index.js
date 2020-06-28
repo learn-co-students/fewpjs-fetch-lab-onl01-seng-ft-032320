ce824769b507dc3e5c420b95e62e37014f6e48e7
ferrisbueller66 <github email address>
2020-06-28 17:19:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function fetchBooks() {
 
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+	.then(function(response) {
+ 		 return response.json();
+	})
+	.then(function(json){
+    renderBooks(json)
+})
 }
 
 function renderBooks(books) {
@@ -11,6 +18,37 @@ function renderBooks(books) {
   })
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+//find 5th book in series
+
+//find 1031st character in the series
+// characters = []
+// fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(json => findCharacters(json));
+
+// function findCharacters(books){books.forEach(book => {characters.push(book.characters)})};
+// function find(characters, criteriaFn) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (criteriaFn(array[i]){ === 1031}) {
+//       return array[i]
+//     }
+//   }
+// }
+
+
+
+
+//find total number of pages:
+// pages = []
+// fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(json => findBook(json));
+
+// function findBook(books){books.forEach(book => {pages.push(book.numberOfPages)})};
+// pages.reduce((a, b) => a + b, 0)
