4c5fdd4eb86ce92d32da509ffa8b25ee10a564dc
rwparrish <rino3717@gmail.com>
2020-06-30 11:42:29 -0600

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function fetchBooks() {
+  //   fetch('https://anapioficeandfire.com/api/books')
+  // .then(function(response) {
+  // return response.json();
+  // }).then(function(json) {
+  //   renderBooks(json);
+  // });
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+} 
 
-}
 
 function renderBooks(books) {
   const main = document.querySelector('main')
